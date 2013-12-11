e5e9d8c80e05a322fd99b4742620cd904725dd31
Reid Peryam <TheFastCat@hotmail.com>
2013-12-11 13:30:16 -0700

    removing unneeded config

--- Startup/Startup.cs
+++ Startup/Startup.cs
@@ -10,7 +10,7 @@ namespace Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseCors(CorsOptions.AllowAll);
             app.UseNancy();
             app.UseStageMarker(PipelineStage.MapHandler);
         }
